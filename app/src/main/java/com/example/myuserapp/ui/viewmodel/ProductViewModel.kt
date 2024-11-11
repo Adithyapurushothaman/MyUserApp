ea177751880dc2ee00379c8b76d4ea92fbfde917
adithya purushothaman <adithyappn@gmail.com>
2024-11-11 14:01:26 +0530

    Refatoring

--- app/src/main/java/com/example/myuserapp/ui/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/myuserapp/ui/viewmodel/ProductViewModel.kt
@@ -1,66 +1,6 @@
-//package com.example.myuserapp.ui.viewmodel
-//
-////class ProductViewModel(private val repository: ProductRepository): ViewModel() {
-////
-////    private val _products = MutableLiveData<List<Product>>()
-////    val products: LiveData<List<Product>> get() = _products
-////
-////    fun fetchProducts(limit: Int, skip: Int) {
-////        viewModelScope.launch {
-////            val response = repository.getProducts(limit, skip)
-////            if (response.isSuccessful) {
-////                _products.postValue(response.body()?.products)
-////            }
-////        }
-////    }
-//////    fun fetchProducts() {
-//////        viewModelScope.launch {
-//////            val response = repository.getProducts()
-//////            if (response.isSuccessful) {
-//////                _products.value = (response.body() ?: emptyList()) as List<Product>?
-//////            }
-//////        }
-//////    }
-////}
-//import androidx.lifecycle.ViewModel
-//import androidx.lifecycle.viewModelScope
-//import com.example.myuserapp.data.models.Product
-//import com.example.myuserapp.data.repository.ProductRepository
-//import kotlinx.coroutines.flow.MutableStateFlow
-//import kotlinx.coroutines.flow.StateFlow
-//import kotlinx.coroutines.launch
-//
-//class ProductViewModel(private val repository: ProductRepository) : ViewModel() {
-//    private val _products = MutableStateFlow<List<Product>>(emptyList())
-//    val products: StateFlow<List<Product>> = _products
-//
-//    private val _selectedProduct = MutableStateFlow<Product?>(null)
-//    val selectedProduct: StateFlow<Product?> = _selectedProduct
-//
-//    private var currentPage = 0
-//    private val limit = 10
-//
-//    init {
-//        loadProducts()
-//    }
-//
-//    fun loadProducts() {
-//        viewModelScope.launch {
-//            val response = repository.getProducts(limit, currentPage * limit)
-//            response.body()?.products?.let { newProducts ->
-//                _products.value += newProducts
-//                currentPage++
-//            }
-//        }
-//    }
-//
-//    fun getProductDetails(id: Int) {
-//        viewModelScope.launch {
-//            val response = repository.getProductDetails(id)
-//            _selectedProduct.value = response.body()
-//        }
-//    }
-//}
+package com.example.myuserapp.ui.viewmodel//package com.example.myuserapp.ui.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
