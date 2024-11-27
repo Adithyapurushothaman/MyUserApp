0251d59a41fe801caec8be4c4d33f8917d4c1b03
adithya purushothaman <adithyappn@gmail.com>
2024-11-27 10:03:35 +0530

    Created a UserViewModel for Logging in and signup of a user and its composable screen.Changes for this is also done in Navigation.kt and in MainActivity.kt.

--- app/src/main/java/com/example/myuserapp/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/myuserapp/data/remote/RetrofitInstance.kt
@@ -5,9 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitInstance {
+    private const val BASE_URL: String = "https://dummyjson.com/"
+
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(" https://dummyjson.com/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
